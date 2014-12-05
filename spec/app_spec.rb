da85bd875fb249bf4c081b236ab7af6c9ad23fc4
Chai Phonbopit <phonbopit@gmail.com>
2014-12-05 14:26:20 +0700

    add test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,15 +3,28 @@ require 'rspec'
 
 describe "My Sinatra Application" do
 
+	describe "GET '/'" do
+	  it "should allow accessing the home page" do
+	    get '/'
+	    # last_response.should be_ok
+	    expect(last_response).to be_ok
+	  end
 
-  it "should allow accessing the home page" do
-    get '/'
-    # last_response.should be_ok
-    expect(last_response).to be_ok
-  end
+	  it "display index content" do
+	  	get '/'
+	  	expect(last_response.body).to include("This is index")
+	  end
+	end
 
-  it "should allow to acess about page" do
-  	get '/about'
-  	expect(last_response).to be_ok
-  end
+	describe "GET '/about'" do
+	  it "should allow to acess about page" do
+	  	get '/about'
+	  	expect(last_response).to be_ok
+	  end
+
+	  it "display about content" do
+	  	get '/about'
+	  	expect(last_response.body).to include("This is Sinatra application")
+	  end
+	end
 end
